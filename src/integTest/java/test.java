c18a663d70e3ad90df322bc64c6564cd5058cb1f
Anastat <anastat@metropolia.fi>
2018-04-17 14:59:55 +0300

    travis integTest

---
+++ src/integTest/java/test.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+
+public class test {
+@Test
+public void dummyIntegrationTest() {
+    assertEquals(0, 1);
+}
+
+@Test
+public void dummyIntegrationTest2() {
+    assertEquals(2, 1);
+}
+}
+
+
